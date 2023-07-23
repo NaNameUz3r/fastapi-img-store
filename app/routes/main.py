136ece8ff3fd3137c99e9b500d5949ca53d6b31c
Ivan Zakutniy <ivan.zakutny@flant.com>
2023-07-23 20:03:53 +0400

    move routes in own submodule and serialize upload response

---
+++ app/routes/main.py
@@ -0,0 +1,58 @@
+import io
+import logging
+import typing as t
+
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi.exceptions import HTTPException
+from starlette import status
+
+from ..storage import MinioService
+from . import serializers
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+minio = MinioService.get_instance()
+
+router = APIRouter()
+
+
+@router.post("/images/")
+async def upload_file(file: t.Optional[UploadFile] = File(None)) -> JSONResponse:
+    if file is not None:
+        try:
+            image_id = minio.put_object(file)
+            return JSONResponse(content=serializers.UploadResponse(
+                ID=image_id,
+                message='Image Uploaded OK').model_dump(),
+                status_code=status.HTTP_201_CREATED
+            )
+
+        except Exception:
+            logger.exception('Occurred unhandled error while processing image')
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Failed to process given message with unhandled error',
+            )
+
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='No file provided',
+    )
+
+
+@router.get("/images/{image_id}")
+async def download(image_id: str):
+    try:
+        image_data = await minio.get_object(image_id)
+        return StreamingResponse(io.BytesIO(image_data), media_type="image/jpg")
+    except:
+        return JSONResponse(status_code=404, content={"message": "Image not found"})
+
+
+@router.delete("/images/{image_id}")
+async def delete(image_id: str):
+    try:
+        await minio.remove_object(image_id)
+    except:
+        return JSONResponse(status_code=404, content={"message": "Image not found"})
